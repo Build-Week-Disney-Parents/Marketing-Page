14a4d9e2e20384d5b4267806df6e029cfc32cb26
Sonica1031 <Sonica1031@hotmail.com>
2019-09-19 19:26:14 -0700

    Working Gallery and some more code

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var clicks = 0;
+Gallery = () =>{
+    if (clicks = 0){
+        let src = "image/stroller 1.jpg";
+    }else if (clicks = 1){
+        let src = "image/stroller 2.jpg";
+    }else if (clicks = 2){
+        let src = "image/stroller 3.jpg";
+    }else{
+        let src = "image/stroller 4.jpg";
+    }
+}
