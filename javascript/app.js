ebc9367102a0c948efdabfb69ab468e7ef5b9cde
Sonica1031 <Sonica1031@hotmail.com>
2019-09-25 17:51:11 -0700

    gallery arrows

--- javascript/app.js
+++ javascript/app.js
@@ -11,8 +11,8 @@ var Click = 0;
         }else if (Click === 3){
                 document.querySelector(".S1").src = "image/stroller4.jpg";    
         }else{
-            Click = 0;
             document.querySelector(".S1").src = "image/stroller1.jpg";
+            Click = 0;
         }
     });
 var Clicker = 0;
@@ -26,8 +26,8 @@ var Clicker = 0;
         }else if (Clicker === 2){
                 document.querySelector(".S1").src = "image/stroller2.jpg";
         }else {
+                document.querySelector(".S1").src = "image/stroller1.jpg";   
                 Clicker = 0;
-                document.querySelector(".S1").src = "image/stroller1.jpg";    
         }
     });
 
