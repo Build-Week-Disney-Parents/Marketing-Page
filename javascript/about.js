c338d6e39818e8f15a3f53ae95379847b5b79ffc
Trevor Thompson <trevthom18@gmail.com>
2019-09-26 21:36:19 -0400

    Revert "Trevor thompson"

--- javascript/about.js
+++ javascript/about.js
@@ -1,37 +1,9 @@
-let imgArray = [];
+var containerProperty= document.getElementsByClassName("container");
 
-imgArray = new Image();
-imgArray[0] = "/images/stephanie.jpg";
+var footerContainer = document.addEventListener("mouseover", (containerProperty) =>{
 
-imgArray = new Image();
-imgArray[1] = "/images/leon.jpg";
-
-imgArray = new Image();
-imgArray[2] = "/images/trevor.jpg";
-
-imgArray = new Image();
-imgArray[3] = "/images/aaron.jpg";
-
-let imageEditor = document.querySelector(".img-formater");
-
-let index = 0;
-
-console.log(imageEditor);
-
-imageEditor = document.addEventListener("click", (imageProperty) =>{
-
-    if(index < 3)
-    {
-        ++index;
-    }
-    else {
-        index = 0;
-    }
+    containerProperty.width = screen.width;
     
-    imageProperty.src = imgArray[index];
-
-    //console.log(imgArray);
-
-    console.log(imageProperty);
+    console.log(containerProperty.width);
 
 });
